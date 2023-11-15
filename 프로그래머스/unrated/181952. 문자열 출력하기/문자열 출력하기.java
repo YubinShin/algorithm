284429a5f5af6b3003b3f8182d94a68789979cb9
Yubin Shin <68121478+YubinShin@users.noreply.github.com>
2023-11-15 15:47:05 +0900

    [level unrated] Title: 문자열 출력하기, Time: 132.70 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        printStr(a);
+    }
+    
+    public static void printStr(String str){
+        System.out.println(str);
+    }
+}
