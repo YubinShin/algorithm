d253cb852b72393980475107f715faaf5d6fe3eb
Yubin Shin <68121478+YubinShin@users.noreply.github.com>
2023-11-16 11:39:04 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sum = 0;
+        int multiply = 1;
+
+        for (int num : num_list) {
+          sum += num;
+          multiply *= num;
+        }
+
+        return sum * sum > multiply ? 1 : 0;
+    }
+}
